eece12dd928c042fa1b3f54a25cde77327cf3e69
evgen <EShutochkin@almazantey.ru>
2018-10-11 11:41:21 +0300

    upd proj

--- task6mycacheengine/src/main/java/cache/CacheEngine.java
+++ task6mycacheengine/src/main/java/cache/CacheEngine.java
@@ -1,24 +1,40 @@
 package cache;
 
-import java.lang.ref.PhantomReference;
+import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.lang.ref.Reference;
 import java.lang.ref.ReferenceQueue;
 import java.lang.ref.SoftReference;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class CacheEngine<K,V> {
 
     private final int maxElements;
     private final Map<K,SoftReference<V>> elements = new HashMap<>();
-    private final List<SoftReference<V>> softReferenceList = new ArrayList<>();
-    private final List<PhantomReferenceItem<K,V>> phantomReferencesList = new ArrayList<>();
     private final ReferenceQueue<V> referenceQueue = new ReferenceQueue<>();
+
+    private final Thread monitorReferenceQueue = new Thread(() -> {
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                synchronized (elements) {
+                    Reference reference = referenceQueue.poll();
+                    if (reference == null) {
+                        Thread.sleep(100);
+                    } else if (reference instanceof SoftReferenceItem) {
+                        SoftReferenceItem softReferenceItem = (SoftReferenceItem) reference;
+                        elements.remove(softReferenceItem.getKey());
+                        softReferenceItem.cleanup();
+                    }
+                }
+            }
+        }catch (Exception ex) {
+            System.out.println("monitorReferenceQueue stop");
+        }
+    });
 
     public CacheEngine(int maxElements) {
         this.maxElements = maxElements;
+        monitorReferenceQueue.start();
     }
 
     public void put(K key, V element) {
@@ -26,18 +42,19 @@ public class CacheEngine<K,V> {
             K firstKey = elements.keySet().iterator().next();
             elements.remove(firstKey);
         }
-        SoftReference<V> softReference = new SoftReference<>(element);
-        PhantomReferenceItem<K,V> phantomReferenceItem = new PhantomReferenceItem<>(
-                key,
-                element,
-                referenceQueue,
-                ()-> { elements.remove(key); });
-        softReferenceList.add(softReference);
-        phantomReferencesList.add(phantomReferenceItem);
+        SoftReference<V> softReference = new SoftReferenceItem<>(key, element, referenceQueue);
         elements.put(key, softReference);
     }
 
     public void printSize() {
+        while (referenceQueue.poll() != null)
+        {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         int cntKey = 0;
         int cntValue = 0;
 
@@ -61,29 +78,15 @@ public class CacheEngine<K,V> {
     }
 
     public void dispose() {
+        monitorReferenceQueue.interrupt();
         elements.clear();
-        for (SoftReference<V> item : softReferenceList) {
-            item.clear();
-        }
-        softReferenceList.clear();
-
-
-        for (PhantomReferenceItem<K,V> item : phantomReferencesList) {
-            item.cleanup();
-            item.clear();
-        }
-        phantomReferencesList.clear();
     }
 
-    private class PhantomReferenceItem<K,T> extends PhantomReference {
+    private class SoftReferenceItem<K,V> extends SoftReference {
         private final K key;
-        private final Runnable destroy;
-        public PhantomReferenceItem(K key,T obj, ReferenceQueue queue, Runnable destroy) {
+        public SoftReferenceItem(K key, V obj, ReferenceQueue queue) {
             super(obj, queue);
             this.key = key;
-            this.destroy = destroy;
-            Thread thread = new ReferenceQueueThread(this,queue);
-            thread.start();
         }
 
         public K getKey() {
@@ -91,37 +94,7 @@ public class CacheEngine<K,V> {
         }
 
         public void cleanup() {
-            destroy.run();
             super.clear();
-        }
-
-        public class ReferenceQueueThread extends Thread {
-
-            private final ReferenceQueue referenceQueue;
-            private final PhantomReferenceItem phantomReference;
-            public ReferenceQueueThread(PhantomReferenceItem phantomReference, ReferenceQueue referenceQueue) {
-                this.referenceQueue = referenceQueue;
-                this.phantomReference = phantomReference;
-            }
-
-            @Override
-            public void run() {
-
-                Reference reference = null;
-
-                while ((reference = referenceQueue.poll()) == null) {
-                    try {
-                        Thread.sleep(100);
-                    }
-                    catch (InterruptedException e) {
-                        throw new RuntimeException("Thread " + getName() + " has been interrupted");
-                    }
-                }
-
-                if (reference instanceof CacheEngine.PhantomReferenceItem && reference != null) {
-                    ((PhantomReferenceItem) reference).cleanup();
-                }
-            }
         }
     }
 }
--- task6mycacheengine/src/main/java/cache/Main.java
+++ task6mycacheengine/src/main/java/cache/Main.java
@@ -1,27 +1,13 @@
 package cache;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-
+    public static void main(String[] args) {
         int size = 250;
-        System.out.println(Thread.activeCount());
         CacheEngine<Integer,Element<Integer, String>> cacheEngine = new CacheEngine<>(size);
-        System.out.println(Thread.activeCount());
-        cacheEngine.printSize();
         for (int i = 0; i < size; i++) {
             cacheEngine.put(i,new Element<>(i, "Element " + i));
         }
-        System.out.println(Thread.activeCount());
         cacheEngine.printSize();
-        System.out.println(Thread.activeCount());
-        //System.gc();
-        //cacheEngine.printSize();
         cacheEngine.dispose();
-        System.out.println(Thread.activeCount());
-        while (true) {
-            Thread.sleep(1000);
-            System.out.println(Thread.activeCount());
-        }
-
     }
 }
