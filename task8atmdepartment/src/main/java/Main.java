db52f83da04c08c690774811e01c22c7b670ab32
evgen <EShutochkin@almazantey.ru>
2018-11-07 15:37:24 +0300

    add proj

---
+++ task8atmdepartment/src/main/java/Main.java
@@ -0,0 +1,35 @@
+/*Написать приложение ATM Department:
+        • Приложение может содержать несколько ATM
+        • Departmant может собирать сумму остатков со всех ATM
+        • Department может инициировать событие – восстановить состояние всех ATM до начального.
+        (начальные состояния у разных ATM могут быть разными)*/
+
+public class Main {
+    public static void main(String[] args) {
+        Atm atm = new Atm();
+
+        atm.deposit(RateСurrency.ONE, 100);
+        atm.deposit(RateСurrency.FIVE, 100);
+        atm.deposit(RateСurrency.TEN, 100);
+        atm.deposit(RateСurrency.HOUNDRED, 100);
+        atm.deposit(RateСurrency.FIVEHOUNDRED, 50);
+        atm.deposit(RateСurrency.THOUSAND, 10);
+        atm.deposit(RateСurrency.FIVETHOUSAND, 5);
+        atm.deposit(RateСurrency.FIVETHOUSAND, 5);
+
+        atm.printStockMoney();
+
+        atm.withdraw(10000);
+        atm.printStockMoney();
+        atm.withdraw(100);
+        atm.printStockMoney();
+        atm.withdraw(10);
+        atm.printStockMoney();
+        atm.withdraw(1);
+        atm.printStockMoney();
+        atm.withdraw(10);
+        atm.printStockMoney();
+        atm.withdraw(3);
+        atm.printStockMoney();
+    }
+}
