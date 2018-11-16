e8adff460730dbcf4401481e56be8c0514129737
evgen <EShutochkin@almazantey.ru>
2018-11-16 16:27:54 +0300

    upd proj

--- task9json/src/main/java/Main.java
+++ task9json/src/main/java/Main.java
@@ -59,41 +59,6 @@ public class Main {
 
 
     }
-/*
-    static long calcSize(Object obj)
-    {
-        return calcSize(obj, null);
-    }
-
-    static long calcSize(Object obj, List<Object> visited) {
-        long result = AgentMemory.sizeof(obj);
-        try {
-            if (visited == null)
-            {
-                visited = new ArrayList<>();
-                visited.add(obj);
-            }
-            Class classInfo = Class.forName(obj.getClass().getName());
-            if (!classInfo.isPrimitive()) {
-                for (Field field : classInfo.getDeclaredFields()) {
-                    if (!Modifier.isStatic(field.getModifiers()) && !field.getType().isPrimitive()) {
-                        field.setAccessible(true);
-                        Object tmpObj = field.get(obj);
-                        if (tmpObj != null && visited.contains(tmpObj) == false) {
-                            result += calcSize(tmpObj, visited);
-                        }
-                    }
-                }
-            }
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
-*/
 }
 
 
