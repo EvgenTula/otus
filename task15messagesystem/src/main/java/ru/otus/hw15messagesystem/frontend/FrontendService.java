f7bcd149d23efb7dc7a1fdb7a0dbd495eceddf99
evgen <evgen.tula@gmail.com>
2019-02-08 10:45:12 +0300

    upd

--- task15messagesystem/src/main/java/ru/otus/hw15messagesystem/frontend/FrontendService.java
+++ task15messagesystem/src/main/java/ru/otus/hw15messagesystem/frontend/FrontendService.java
@@ -3,8 +3,6 @@ package ru.otus.hw15messagesystem.frontend;
 import ru.otus.hw15messagesystem.messagesystem.Sender;
 import ru.otus.hw15messagesystem.websocket.DBServiceWebSocket;
 
-import java.io.IOException;
-
 public interface FrontendService extends Sender {
     void sendDataClient(String uuid, String data);
     String addClient(DBServiceWebSocket webSocket);
